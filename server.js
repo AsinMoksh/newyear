24320b28168bbeb4b53e3279c2607a3c42076393
Asin <mokshnaik21@gmail.com>
2026-01-01 00:32:50 +0530

    Initial commit including node_modules

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    socket.room = room;
+  });
+
+  socket.on("chat", (msg) => {
+    io.to(socket.room).emit("chat", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("🎉 Server running at http://localhost:3000");
+});
